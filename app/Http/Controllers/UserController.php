4ec7c848d674ed88cfe9412bb1e4d946be5c8ab7
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2021-10-01 23:33:40 +0300

    ff

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class UserController extends Controller
+{
+
+    public function login(){
+
+        Session::put('user_id', 1);
+        return redirect()->route('project.index');
+    }
+}
